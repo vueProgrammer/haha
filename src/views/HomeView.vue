be718a6599194f7ecdb42468e46690945d423b56
赵崇亮 <congliang.zhao@yuwantech.com>
2023-02-03 15:14:29 +0800

    打包优化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="bottom">
+      <p>备案号: <a href="https://beian.miit.gov.cn/">京ICP备2022018021号-1</a> </p>
+    </div>
   </div>
 </template>
 
@@ -16,3 +19,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.bottom {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #000;
+  padding: 30px 0;
+  font-size: 24px;
+  color: #fff;
+  text-align: center;
+}
+</style>
